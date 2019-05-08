2fc73a754a7fa6dd2904ee79c9dd601e53323598
linhaoIT <linhao.it@gmail.com>
2019-05-09 04:41:37 +1000

    add function

--- src/components/AddTags.vue
+++ src/components/AddTags.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="add-tag container">
     <h2 class="center-align indigo-text">Add New Tag</h2>
-    <form @submit.prevent="AddTag">
+    <form @submit.prevent="AddTags">
       <div class="field title">
         <label for="title">Tag Title:</label>
-        <input type="text" name="title" v-model="title">
+        <input type="text" name="title" id="title" v-model="title">
+      </div>
+      <div v-for="(ing, index) in ingredients" :key="index">
+        <label for="ingredient">Ingredient:</label>
+        <input type="text" name="ingredient" id="ingredient" v-model="ingredients[index]">
       </div>
       <div class="field add-ingredient">
         <label for="add-ingredient"> Add-ingredient</label>
-        <input type="text" name="ingredient">
+        <input type="text" name="ingredient" id="add-ingredient" @keydown.tab.prevent="AddIng" v-model="another">
       </div>
       <div class="field center-align">
+        <p v-if="feedback" class="red-text">{{ feedback }}</p>
         <button class="btn pink">Add Tag</button>
       </div>
     </form>
@@ -18,14 +23,37 @@
 </template>
 
 <script>
+  import db from '@/firebase/init'
     export default {
         name: "addTags",
         data() {
-          return {}
+          return {
+            title: null,
+            another: null,
+            ingredients: [],
+            feedback: null,
+            slug: null
+          }
         },
         methods:{
-          AddTag(){
-
+          AddTags(){
+            if(this.title){
+              db.collection('tags').add({
+                title: this.title,
+                ingredients: this.ingredients
+              })
+            }else{
+              this.feedback = 'You must have a title'
+            }
+          },
+          AddIng(){
+            if(this.another){
+              this.ingredients.push(this.another);
+              this.another = null;
+              this.feedback = null
+            }else{
+              this.feedback = 'You must entre a value to add an ingredient'
+            }
           }
         }
     }
