ae7933c0a11a7ec3021b2314c892ac3e0fafcbc5
linhaoIT <linhao.it@gmail.com>
2019-05-09 06:40:44 +1000

    final

--- src/components/EditTag.vue
+++ src/components/EditTag.vue
@@ -1,25 +1,84 @@
 <template>
     <div v-if="tag" class="edit-tag container">
       <h2 >Edit {{tag.title }} Tag</h2>
+      <form @submit.prevent="EditTags">
+        <div class="field title">
+          <label for="title">Tag Title:</label>
+          <input type="text" name="title" id="title" v-model="tag.title">
+        </div>
+        <div class="field" v-for="(ing, index) in tag.ingredients" :key="index" >
+          <label for="ingredient">Ingredient:</label>
+          <input type="text" name="ingredient" id="ingredient" v-model="tag.ingredients[index]">
+          <i class="material-icons delete" @click="deleteIng(ing)">delete</i>
+        </div>
+        <div class="field add-ingredient">
+          <label for="add-ingredient"> Add-ingredient</label>
+          <input type="text" name="ingredient" id="add-ingredient" @keydown.tab.prevent="AddIng" v-model="another">
+        </div>
+        <div class="field center-align">
+          <p v-if="feedback" class="red-text">{{ feedback }}</p>
+          <button class="btn pink">Update Tag</button>
+        </div>
+      </form>
     </div>
 </template>
 
 <script>
   import db from '@/firebase/init'
+  import slugify from 'slugify'
 
     export default {
         name: "EditTag",
         data(){
           return{
-            tag: null
+            tag: null,
+            another: null,
+            feedback: null
           }
         },
+      methods:{
+          EditTags(){
+              if(this.tag.title){
+                this.feedback = null;
+                this.slug = slugify(this.tag.title, {
+                  replacement: '-',
+                  remove: /[$*_+.()'"!-:@]/g,
+                  lower:true
+                });
+                db.collection('tags').doc(this.tag.id).update({
+                  title: this.tag.title,
+                  ingredients: this.tag.ingredients,
+                  slug: this.tag.slug
+                }).then(()=>{
+                  this.$router.push({name: 'Index'})
+                }).catch(err=>{
+                  console.log(err)
+                })
+              }else{
+                this.feedback = 'You must have a title'
+              }
+            },
+        AddIng(){
+          if(this.another){
+            this.tag.ingredients.push(this.another);
+            this.another = null;
+            this.feedback = null
+          }else{
+            this.feedback = 'You must enter a value to add an ingredient'
+          }
+        },
+        deleteIng(ing){
+          this.tag.ingredients = this.tag.ingredients.filter(ingredient => {
+            return ingredient !== ing
+          })
+        }
+      },
       created(){
-          let ref = db.collection('tags').where('slug', '==', this.$route.params.tag_slug)
+          let ref = db.collection('tags').where('slug', '==', this.$route.params.tag_slug);
           ref.get().then(snapshot => {
             snapshot.forEach(doc => {
               //console.log(doc.data())
-              this.tag = doc.data()
+              this.tag = doc.data();
               this.tag.id = doc.id
             })
           })
@@ -28,5 +87,28 @@
 </script>
 
 <style scoped>
+  .edit-tag{
+    margin-top: 60px;
+    padding: 20px;
 
+  }
+
+  .edit-tag h2{
+    font-size: 2em;
+    margin: 20px auto;
+  }
+
+  .edit-tag .field{
+    margin: 20px auto;
+    position: relative;
+  }
+
+  .edit-tag .delete{
+    position: absolute;
+    right:0;
+    bottom:16px;
+    color: #aaa;
+    font-size: 1.4em;
+    cursor: pointer;
+  }
 </style>
