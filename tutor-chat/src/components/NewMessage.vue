7657f57bece71515cb59b36eed172d8fb52b6637
linhaoIT <linhao.it@gmail.com>
2019-05-09 19:05:42 +1000

    toTheMessagePart

---
+++ tutor-chat/src/components/NewMessage.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="new-message">
+      <form @submit.prevent="addMessage"></form>
+        <label for="new-message">New Message (enter to add): </label>
+        <input type="text" name="new-message" id="new-message" v-model="newMessage">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NewMessage",
+      props:['name'],
+        data(){
+          return{
+            newMessage: null
+          }
+        },
+      methods:{
+          addMessage(){
+
+          }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
