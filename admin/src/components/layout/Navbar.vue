59f9765ef3f6bb36d9b0b5360964f34a554c9442
linhaoIT <linhao.it@gmail.com>
2019-05-09 22:44:01 +1000

    map

---
+++ admin/src/components/layout/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="navbar">
+      <nav class="#2979ff blue accent-3">
+        <div class="container">
+          <a href="" class="brand-logo left">RMC</a>
+          <ul class="right">
+            <li><a href="">Sign up</a></li>
+            <li><a href="">Login</a></li>
+          </ul>
+          </div>
+      </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Navbar",
+        data(){
+          return {
+
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
