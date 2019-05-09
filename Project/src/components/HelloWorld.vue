3a2a2b41939a23d0b76bc217060c82fca7ecfcf0
linhaoIT <linhao.it@gmail.com>
2019-05-10 05:36:10 +1000

    first page

--- Project/src/components/HelloWorld.vue
+++ Project/src/components/HelloWorld.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
+  <div class="homePage">
+    <div class="navbar">
+      <nav class="#0288d1 light-blue darken-2">
+        <div class="container">
+          <a href="" class="left">
+            <img src= '../assets/icon.png' alt="icon: rate my course" class="icon">
+          </a>
+          <div class="webname">Rate My Course System</div>
+        </div>
+      </nav>
+    </div>
+    <img class="slogan" src="../assets/slogan.png" alt="slogan">
+    <a class="Signup waves-effect waves-light btn-large #0288d1 light-blue darken-2" href=""><i class="material-icons left">assignment</i>Sign up</a>
+    <a class="Login waves-effect waves-light btn-large #0288d1 light-blue darken-2" href=""><i class="material-icons left">assignment_ind</i>Log in</a>
 
   </div>
 </template>
@@ -12,10 +25,57 @@
       return{
 
       }
+    },
+    components:{
+
     }
   }
 </script>
 
+
 <style scoped>
+  .icon {
+    width: 70px;
+    height: auto;
+
+    /*//background: url('@/assets/icon.jpg') center center no-repeat;*/
+    /*background-size: 100px auto;*/
+    /*}*/
+  }
+
+  .homePage{
+    position: relative;
+    background-image: url("../assets/light.jpg");
+    background-color: #cccccc;
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 820px;
+    background-position: center;
+  }
+
+  .slogan{
+    position: relative;
+    height: 350px;
+    left: 180px;
+    top: 100px;
+  }
+  .webname{
+    font-family: "Comic Sans MS";
+    font-size: x-large;
+    color: white;
+  }
+
+  .Signup{
+    position: relative;
+    right: 340px;
+    top: 170px;
+  }
+
+  .Login{
+    position: relative;
+    right: 260px;
+    top: 170px;
+
+  }
 
 </style>
--- admin/src/components/layout/Navbar.vue
+++ admin/src/components/layout/Navbar.vue
@@ -1,40 +1,40 @@
 <template>
-    <div class="navbar">
-      <nav class="#2979ff blue accent-3">
-        <div class="container">
-          <img src= '../../assets/icon.png'  alt="icon: rate my course" class="icon">
-          <a href="" class="icon left">RMC</a>
-          <ul class="right">
-            <li><a href="">Sign up</a></li>
-            <li><a href="">Login</a></li>
-          </ul>
-        </div>
-      </nav>
-    </div>
+  <div class="navbar">
+    <nav class="#2979ff blue accent-3">
+      <div class="container">
+        <img src= '../../assets/icon.png'  alt="icon: rate my course" class="icon">
+        <a href="" class="icon left">RMC</a>
+        <ul class="right">
+          <li><a href="">Sign up</a></li>
+          <li><a href="">Login</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script>
   //import icon from
-    export default {
-        name: "Navbar",
-        data(){
-          return {
+  export default {
+    name: "Navbar",
+    data(){
+      return {
 
-          }
-        },
-        components:{
-          //icon
-        }
+      }
+    },
+    components:{
+      //icon
     }
+  }
 </script>
 
 <style scoped>
-.icon {
-  width: 70px;
-  height: auto;
+  .icon {
+    width: 70px;
+    height: auto;
 
-  /*//background: url('@/assets/icon.jpg') center center no-repeat;*/
-  /*background-size: 100px auto;*/
-  /*}*/
-}
+    /*//background: url('@/assets/icon.jpg') center center no-repeat;*/
+    /*background-size: 100px auto;*/
+    /*}*/
+  }
 </style>
