a2518faced862ebae822e86950c3e23cf9a2e094
linhaoIT <linhao.it@gmail.com>
2019-05-09 23:42:58 +1000

    signup

--- admin/src/components/auth/Signup.vue
+++ admin/src/components/auth/Signup.vue
@@ -1,13 +1,61 @@
 <template>
-    
+   <div class="signup container">
+     <form @submit.prevent="signup" class="card-panel">
+       <h2 class="center deep-purple-text">Signup</h2>
+       <div class="field">
+         <label for="name">Name:</label>
+         <input type="text" id="name" v-model="name">
+       </div>
+
+       <div class="field">
+         <label for="password">Password:</label>
+         <input type="password" id="password" v-model="password">
+       </div>
+
+       <div class="field">
+         <label for="email">Email:</label>
+         <input type="email" id="email" v-model="email">
+       </div>
+       <div class="field center"></div>
+       <button class="btn deep-purple">Signup</button>
+     </form>
+   </div>
 </template>
 
 <script>
     export default {
-        name: "Signup"
+        name: "Signup",
+      data(){
+          return{
+            email: null,
+            password: null,
+            name: null,
+            feedback: null
+          }
+      },
+      methods:{
+          signup(){
+            if(this.name){
+
+            }else{
+              this.feedback = "You must "
+            }
+          }
+      }
     }
 </script>
 
 <style >
+.signup{
+  max-width: 400px;
+  margin-top:60px;
+}
+  .signup h2{
+    font-size: 2.4em;
+  }
+
+  .signup .field{
+    margin-bottom: 16px;
+  }
 
 </style>
--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Map from '@/components/home/Map'
+import Signup from '@/components/auth/Signup'
 
 Vue.use(Router)
 
@@ -11,8 +12,10 @@ export default new Router({
       name: 'Map',
       component: Map
     },
-    // {
-    //
-    // }
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup
+    }
   ]
 })
