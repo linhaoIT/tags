af10966f0d4b13cc794523a6556b1aee15186516
linhaoIT <linhao.it@gmail.com>
2019-05-10 07:20:45 +1000

    login

--- admin/src/components/auth/Login.vue
+++ admin/src/components/auth/Login.vue
@@ -1,4 +1,6 @@
 <template>
+  <div>
+    <Navbar/>
     <div class="login container">
       <form class="card-panel">
         <h2 class="center blue-text">Login</h2>
@@ -11,20 +13,46 @@
             <label for="password">Password:</label>
             <input type="password" id="password" v-model="password">
           </div>
+            <p v-if="feedback" class="red-rext center">{{ feedback }}</p>
             <div class="field">
               <button class="btn #0288d1 light-blue darken-2"> Login</button>
             </div>
         </div>
       </form>
     </div>
+  </div>
 </template>
 
 <script>
+    import Navbar from "@/components/layout/Navbar";
     export default {
-        name: "Login.vue"
+        name: "Login.vue",
+      components: {Navbar},
+      data(){
+          return {
+            email: null,
+            password: null,
+            feedback: null
+          }
+        },
+      methods:{
+          login(){
+            console.log(this.email, this.password)
+          }
+      }
     }
 </script>
 
 <style scoped>
+.login{
+  max-width: 400px;
+  margin-top: 60px;
+}
+  .login h2{
+    font-size: 2.4em;
+  }
 
+  .login ,field{
+    margin-bottom: 16px;
+  }
 </style>
--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Map from '@/components/home/Map'
 import Signup from '@/components/auth/Signup'
+import Login from '@/components/auth/Login'
 import Home from '@/components/Home'
 
 Vue.use(Router)
@@ -22,6 +23,11 @@ export default new Router({
       path: '/signup',
       name: 'Signup',
       component: Signup
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
